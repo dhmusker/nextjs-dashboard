de027056602300bc5c05fa03ac4cf0b0c4307374
dhmusker <dhmusker@gmail.com>
2024-08-26 06:48:16 +1000

    fixed error by commenting seed file

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -2,7 +2,7 @@ import bcrypt from 'bcrypt';
 import { db } from '@vercel/postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
-const client = await db.connect();
+/* const client = await db.connect();
 
 async function seedUsers() {
   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
@@ -100,13 +100,13 @@ async function seedRevenue() {
 
   return insertedRevenue;
 }
-
+ */
 export async function GET() {
-  // return Response.json({
-  //   message:
-  //     'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  // });
-  try {
+  return Response.json({
+    message:
+      'Uncomment this file and remove this line. You can delete this file when you are finished.',
+  });
+/*   try {
     await client.sql`BEGIN`;
     await seedUsers();
     await seedCustomers();
@@ -118,5 +118,5 @@ export async function GET() {
   } catch (error) {
     await client.sql`ROLLBACK`;
     return Response.json({ error }, { status: 500 });
-  }
+  } */
 }
